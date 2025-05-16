6ac84b84129e3b3ccf4ee7f81a6ca3715bbe19f1
Misbah1992 <misbah199219@gmail.com>
2025-05-16 19:12:16 +0500

    responsive in progress

--- src/components/transportSection/TransportSection.jsx
+++ src/components/transportSection/TransportSection.jsx
@@ -13,7 +13,7 @@ function TransportSection() {
         <div className='bg-wjite absolute z-10 h-[85%] w-[70%] flex flex-col items-center justify-between'>
           <div className='text-center'>
             <div className='text-xs text-white font-bold'>LOGISTICS EXPERTS</div>
-            <div className='text-4xl text-white font-semibold'>Transport solutions</div>
+            <div className='2xl:text-4xl xl:text-3xl md:text-2xl text-white font-semibold'>Transport solutions</div>
             <div className='text-sm text-white font-light my-4'>Sed ut perspiciatis unde omnis iste natus ut perspic iatis unde<br />
               omnis iste perspiciatis ut perspiciatis.</div>
           </div>
@@ -23,23 +23,23 @@ function TransportSection() {
           </div>
           <div className='flex w-[100%] justify-between items-center'>
             <div className=' flex items-center justify-center'>
-              <div className='text-white text-lg font-semibold absolute'>Projects</div>
-              <div className='text-gray-800 text-9xl font-semibold'>98</div>
+              <div className='text-white 2xl:text-lg xl:text-md font-semibold absolute'>Projects</div>
+              <div className='text-gray-800 2xl:text-9xl xl:text-8xl lg:text-7xl font-semibold'>98</div>
             </div>
             <div className='h-12 w-1 bg-gray-800'></div>
             <div className=' flex items-center justify-center'>
-              <div className='text-white text-lg font-semibold absolute'>People</div>
-              <div className='text-gray-800 text-9xl font-semibold'>65</div>
+              <div className='text-white text-lg xl:text-md font-semibold absolute'>People</div>
+              <div className='text-gray-800 2xl:text-9xl xl:text-8xl lg:text-7xl font-semibold'>65</div>
             </div>
             <div className='h-12 w-1 bg-gray-800'></div>
             <div className=' flex items-center justify-center'>
-              <div className='text-white text-lg font-semibold absolute'>Years</div>
-              <div className='text-gray-800 text-9xl font-semibold'>10</div>
+              <div className='text-white text-lg xl:text-md font-semibold absolute'>Years</div>
+              <div className='text-gray-800 2xl:text-9xl xl:text-8xl lg:text-7xl font-semibold'>10</div>
             </div>
             <div className='h-12 w-1 bg-gray-800'></div>
             <div className=' flex items-center justify-center'>
-              <div className='text-white text-lg font-semibold absolute'>Offices</div>
-              <div className='text-gray-800 text-9xl font-semibold'>15</div>
+              <div className='text-white text-lg xl:text-md font-semibold absolute'>Offices</div>
+              <div className='text-gray-800 2xl:text-9xl xl:text-8xl lg:text-7xl font-semibold'>15</div>
             </div>
           </div>
         </div>
